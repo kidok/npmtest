dbad3dd1ee2a36385a2a18620f5268e3b6858629
James Chen <kidokchen@gmail.com>
2015-03-13 17:13:54 -0700

    Update temp.js

--- connecttest/temp.js
+++ connecttest/temp.js
@@ -1,104 +1,79 @@
-<!doctype html>
-<html ng-app="productsApp">
+nettest server.js
+var net = require('net');
+var fs = require('fs');
 
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-resource.js"></script>
-    <script src="product.js"></script>
-</head>
+var server = net.createServer(function(c){
+	fs.writeFile('serverobj.txt', c.toString(), function(){
+		console.log("file write done");
+	});
 
-<body>
-    <h2>Products</h2>
-    <div ng-controller="ProductsController">
-        <form ng-submit="addProduct()">
-            <input type="text" ng-model="name" size="30" placeholder="add new product here">
-            <input class="btn-primary" type="submit" value="add">
-        </form>
-        <ul>
-            <li ng-repeat="product in products">            
-                <span>ID: {{product._id}}</span>
-                <br>
-                <span>Name: {{product.name}}</span>
-                <br>
-                Delete<input type="checkbox" value="delete" ng-click="deleteProduct(product._id)">
-                <br>
-                Edit<input type="checkbox" value="edit" onclick="this.checked?this.nextSibling.style.display = 'block':this.nextSibling.style.display = 'none'"><div style="display:none">
-                <input type="text" ng-model="product.name"><input class="btn-primary" type="submit" value="edit" ng-click="updateProduct(product._id)">
-                </div>
-            </li>
-        </ul>
-    </div>
-</body>
+	console.log("client connected");
+	c.on('end', function(){
+		console.log("client disconected");
+	});
 
-</html>
+	c.write('hello');
+	c.pipe(c);
+});
 
-(function(angular) {
-    'use strict';
-    var productServices = angular.module('productServices', ['ngResource']);
-
-    productServices.factory('Product', ['$resource', function($resource) {
-        return $resource('http://localhost:3000/api/products/:id', {}, {
-            'update': {
-                method: 'PUT',
-                params: {
-                    id: '@id'
-                }
-            }
-        });
-    }])
-
-    angular.module("productsApp", ["productServices"])
-        .controller('ProductsController', ['$scope', 'Product', function($scope, Product) {
-            $scope.addProduct = function() {
-                var pp = {
-                    name: $scope.name,
-                    price: 100.00
-                }
-                Product.save(pp);
-                $scope.products = Product.query();
-            };
-            $scope.deleteProduct = function(id) {
-                //console.log(id);
-                Product.delete({
-                    id: id
-                });
-                $scope.products = Product.query();
-            }
-            $scope.updateProduct = function(product) {
-                var pp = {
-                    name: $scope.name,
-                    price: 100.00
-                }
-                console.log(product);
-                Product.update({
-                    id: product._id
-                }, product);
-                $scope.products = Product.query();
-            }
-            $scope.products = Product.query();
-        }])
-})(window.angular)
+server.listen(8124, function(){
+	console.log("server bound");
+});
 
 
-var express = require("express");
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+client.js
+var net = require('net');
+var option = {
+	port: 8124
+}
+var client = net.connect(option, function(){
+	console.log("connected to server");
+	client.write("world");
+});
 
-var app = express();
+client.on('data', function(data){
+	console.log(data.toString());
+	client.end();
+});
 
-mongoose.connect("mongodb://localhost/rest_test");
+client.on('end', function(){
+	console.log("disconnnected from server");
+});
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use('/api', require('./routers/api'));
-
-app.use(express.static(__dirname + '/public'));
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+client.on('error', function(){
+	console.log("error happened on client si");
 })
+
+
+websocket.js
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets server
+    // we don't have to implement anything.
+});
+server.listen(1337, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+        }
+        console.log(message);
+    });
+
+    connection.on('close', function(connection) {
+        // close user connection
+    });
+});
