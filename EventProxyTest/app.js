8204d53593a71d5ba50fc85e65db4a71f1e158b0
James Chen <jameschen@Jamess-MacBook-Pro.local>
2015-06-16 21:55:21 -0700

    add

---
+++ EventProxyTest/app.js
@@ -0,0 +1,24 @@
+var EventProxy = require('eventproxy');
+var ep = new EventProxy();
+var fs = require('fs');
+
+ep.all('reada1','reada2', function(c1, c2){
+	console.log('a1 and a2 are all done');
+	console.log(c1 + c2);
+});
+
+fs.readFile('a1.txt', function(err, content) {
+
+	setTimeout(function(content){
+		console.log(content.toString('utf8'));
+		ep.emit('reada1', content);
+	}, 3000, content);
+	
+    
+    console.log('reada1 done');
+});
+	
+fs.readFile('a2.txt', function(err, content) {
+    ep.emit('reada2', content);
+    console.log('reada2 done');
+});
