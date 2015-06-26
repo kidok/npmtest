9cb1ac8f6755e906724dc4577bcb32cc14dc6b0e
James Chen <jameschen@Jamess-MacBook-Pro.local>
2015-06-25 23:36:27 -0700

    add

--- CrawlerTest/app.js
+++ CrawlerTest/app.js
@@ -1,12 +1,29 @@
 var Crawler = require("simplecrawler");
-
 var myCrawler = new Crawler("localhost", "/info/1", 3002);
+var pageCount = 0;
+var totalCrawlerPages = 500;
 
 myCrawler.maxDepth = 2;
+myCrawler.interval = 500;
 
-myCrawler.on("fetchcomplete", function(queueItem, data){
-        console.log("Completed fetching resource:", queueItem.url);
-        //console.log(data.toString('utf8'));
-    });
+myCrawler.on("fetchcomplete", function(queueItem, data) {
+    console.log("Completed fetching resource:", queueItem.url);
+    //console.log(data.toString('utf8'));
+    pageCount++;
+    if(pageCount == totalCrawlerPages){
+    	console.log(myCrawler.queue)
+		process.exit();   	
+    }
+});
+
+myCrawler.on('complete', function() {
+	console.log(myCrawler.queue)
+    console.log('page count:' + pageCount);
+
+});
+
+var conditionID = myCrawler.addFetchCondition(function(parsedURL) {
+    return !(parsedURL.path.match(/\.jpg$/i) || parsedURL.path.match(/\.js$/i) || parsedURL.path.match(/\.css$/i));
+});
 
 myCrawler.start();
