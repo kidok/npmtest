bbf77d4c41d90ab9b6b010ee1dddd32e35050523
kidok <kidokchen@gmail.com>
2016-06-17 14:31:45 -0700

    a

--- ExpressTest/TemplateEngine/hogantest/app.js
+++ ExpressTest/TemplateEngine/hogantest/app.js
@@ -7,15 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var test = require('./routes/test');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', require('ejs').__express);
-app.engine('ejs', require('ejs').__express);
+app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
